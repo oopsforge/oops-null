ea9d68ec3d116a894c2dd0d5b9aa1b80cbe11363
Jon <jon.forums@gmail.com>
2011-01-26 15:42:39 -0500

    Initial hack to add debug symbols to 1.8 builds.

--- ext/oops_null/extconf.rb
+++ ext/oops_null/extconf.rb
@@ -1,10 +1,15 @@
 require 'mkmf'
 
-# stop LDSHARED stripping symbols (-s) if debugflags only
-# contains -g and/or -g0
+# Stop 1.9.2's LDSHARED from stripping symbols (-s) if debugflags
+# only contains -g and/or -g0 and remove 1.8.7 LDSHARED's hard-coded
+# -s
 if ENV['BUILD_DEBUG_LIB']
   puts '[INFO] enabling debug library build configuration.'
-  CONFIG['debugflags'] << ' -ggdb3 -O0'
+  if RUBY_VERSION < '1.9'
+    CONFIG['LDSHARED'] = 'gcc -shared'
+  else
+    CONFIG['debugflags'] << ' -ggdb3 -O0'
+  end
 end
 
 create_makefile('oops_null/oops_null')
